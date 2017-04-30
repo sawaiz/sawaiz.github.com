8440a536edf8370bc3e36a48229767c24ee080a2
sawaiz <github@sawaizsyed.com>
2017-04-30 14:39:10 -0400

    Attempted adding josh.js terminal. Found non jquery version mass:werk termlib.js so i'll try that

---
+++ resume/education.js
@@ -0,0 +1,13 @@
+// Education object
+// Push education in cronological order
+
+var education = [];
+
+education.push({
+    instittuion: "Georgia State University",
+    url: "http://www.gsu.edu/",
+    degree: "Bachelor of Science",
+    concentration: "Physics",
+    location: "Atlanta, GA",
+    graduation: "2015"
+});
---
+++ resume/skills.js
@@ -0,0 +1,17 @@
+var skills = {};
+
+skills.technical = [];
+skills.programing = [];
+skills.softwarePacakges = [];
+skills.naturalLanguages = [];
+
+skils.technical.push({
+    skill: "Electronics Design and Protoyping",
+    description: "Circuit design, layout, soldering and assembly",
+    hours: 15000,
+});
+
+skills.technical.push({
+    skill: "Circuit Design"
+});
+
